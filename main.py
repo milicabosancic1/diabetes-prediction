6ea503f108e00034bd68e2b0b3664ea358a99920
milicabosancic1 <milicabosancic1612@gmail.com>
2025-10-15 19:13:49 +0200

    upgraded main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# main.py
 import os
 import sys
+
+from src.plotting import generate_all_plots
 from src.train_eval import train_and_evaluate_all
 
 BASE_DIR = os.path.dirname(__file__)
@@ -26,6 +27,7 @@ def main() -> int:
         csv_path = resolve_csv_path()
         out_dir = os.path.join(BASE_DIR, "outputs")
         train_and_evaluate_all(csv_path=csv_path, out_dir=out_dir, seed=42)
+        generate_all_plots(outputs_dir=os.path.join(BASE_DIR, 'outputs'))
         print("✅ Gotovo. Rezultati su u 'outputs/'.")
         return 0
     except Exception as e:
